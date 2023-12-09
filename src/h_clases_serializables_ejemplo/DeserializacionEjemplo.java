9c1736126f4663184d42ceba4922caaf0943788f
Alejandro Alonso <aalonsopuig@gmail.com>
2023-12-09 10:58:38 +0100

    .

--- src/h_clases_serializables_ejemplo/DeserializacionEjemplo.java
+++ src/h_clases_serializables_ejemplo/DeserializacionEjemplo.java
@@ -1,5 +1,21 @@
 package h_clases_serializables_ejemplo;
 
+/*
+ * Ejemplo de serialización de un objeto Persona.
+ * Explicación:
+ * - Creación de ObjectInputStream: Usamos ObjectInputStream para leer la secuencia de bytes 
+ *   del archivo persona.ser.
+ * - Deserialización del Objeto: Con el método readObject() de ObjectInputStream, leemos y 
+ *   convertimos la secuencia de bytes almacenada en el archivo de nuevo en un objeto de la 
+ *   clase Persona. Esta operación requiere un casting explícito.
+ * - Manejo de Excepciones: El proceso de deserialización puede lanzar dos tipos de excepciones: 
+ *   IOException (si hay problemas de E/S) y ClassNotFoundException (si la clase del objeto 
+ *   serializado no se encuentra en el classpath).
+ * - Uso del Objeto Deserializado: Una vez deserializado el objeto, puedes usar sus métodos 
+ *   como lo harías con cualquier otro objeto. En este ejemplo, estamos imprimiendo los valores 
+ *   de los atributos del objeto Persona deserializado.
+ */
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
--- src/h_clases_serializables_ejemplo/SerializacionEjemplo.java
+++ src/h_clases_serializables_ejemplo/SerializacionEjemplo.java
@@ -1,5 +1,16 @@
 package h_clases_serializables_ejemplo;
 
+/*
+ * Ejemplo de serialización de un objeto Persona.
+ * Explicación:
+ * - Implementación de Serializable: La clase Persona implementa la interfaz Serializable, lo que permite 
+ *   que sus instancias sean convertidas a un flujo de bytes y viceversa.
+ * - Campo serialVersionUID: Es un identificador único que se utiliza durante la deserialización para 
+ *   verificar que el remitente y el receptor de un objeto serializado mantienen la compatibilidad.
+ * - Serialización: En el método main, creamos una instancia de Persona y usamos ObjectOutputStream para 
+ *   escribir este objeto en un archivo (persona.ser).
+ */
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
